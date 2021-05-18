fa3472fd077f284062e29482e483ca239eaa7633
JohnS-CIS245 <you@example.com>
2021-05-17 21:45:41 -0400

    Commit Assignment10-1

---
+++ Assignment10_1.py
@@ -0,0 +1,92 @@
+'''
+John Savarese
+Week 10
+Assignment 10.1
+CIS245-T302-T801
+May 17, 2021
+
+
+This week we will create a program that performs file processing activities.  
+Your program this week will use the OS library in order to validate that a directory exists before creating a file in that directory.  
+Your program will prompt the user for the directory they would like to save the file in as well as the name of the file.  
+The program should then prompt the user for their name, address, and phone number.  
+Your program will write this data to a comma separated line in a file and store the file in the directory specified by the user. 
+Once the data has been written your program should read the file you just wrote to the file system and display the file contents to the user for validation purposes. 
+Submit a link to your Github repository.
+'''
+import json
+import os
+
+def get_user_information():
+
+    name = input ("\nPlease enter your full name (example : Jane Doe) : ")
+    address = input ("\nPlease enter your address (example : 123 Barber Street) : ")
+    city = input ("\nPlease enter your city name (example : Greentown) : ")
+    state = input ("\nPlease enter your state (example : Idaho) : ")
+    zipcode = input ("\nPlease enter your zip code (example : 74502) : ")
+    phonenumber = input ("\nPlease enter your phone number (example : 555-555-5555) : ")
+
+    personalinformation = [name, address, city, state, zipcode, phonenumber]
+    return personalinformation
+
+def get_directory_name():
+
+    directoryinput = input ("\nPlease enter the directory that you would like to save your file to (example : C:\MyInformation) : ")
+    try :
+        os.mkdir(directoryinput)
+        print (f"\nDirectory : {directoryinput} created...")
+    except : 
+        print (f"\nOk, that directory already exists, let us continue...")
+    return directoryinput
+
+def get_file_name(directoryinput):
+
+    filename = input ("\nPlease enter a file name to save your information to (example : filename) : ")
+    path_file = directoryinput + '/' + filename + ".json"
+
+    try : 
+        with open (path_file) as f:
+            filename = input ("\nThat file already exists, please enter a new file name to save your information to (example : filename) : ")
+            path_file = directoryinput + '/' + filename + ".json"
+    except FileNotFoundError:
+        print (f"\nOk, your information will be saved as : {path_file}, let us continue...")
+    
+    return path_file
+
+def write_file(personalinfo,path_file):
+
+    with open (path_file, 'w') as f :
+        json.dump(personalinfo, f)
+
+def open_file(path_file):
+
+    with open (path_file) as f :
+        information = json.load(f)
+        return information
+
+def print_information_from_file (information,path_file):
+
+    print ("\nThe information you provided to be saved is as follows : ")
+
+    for info in information :
+        print (info.title())
+
+    print (f"\nYour information has been stored in : {path_file}\n")
+
+def main ():
+
+    directoryinput = get_directory_name()
+    path_file = get_file_name(directoryinput)
+    personalinfo = get_user_information()
+    write_file(personalinfo,path_file)
+    information = open_file(path_file)
+    print_information_from_file(information,path_file)
+
+main()
+
+
+
+
+
+
+
