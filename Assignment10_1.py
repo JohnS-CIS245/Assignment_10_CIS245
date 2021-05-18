d4f601a063a19a68c5cfb36d330ef11f6a872c40
JohnS-CIS245 <you@example.com>
2021-05-17 22:03:20 -0400

    commit assignment10.1 with comments

--- Assignment10_1.py
+++ Assignment10_1.py
@@ -14,10 +14,10 @@ Your program will write this data to a comma separated line in a file and store 
 Once the data has been written your program should read the file you just wrote to the file system and display the file contents to the user for validation purposes. 
 Submit a link to your Github repository.
 '''
-import json
-import os
+import json #Import json
+import os #Import os
 
-def get_user_information():
+def get_user_information(): #Function to take user information
 
     name = input ("\nPlease enter your full name (example : Jane Doe) : ")
     address = input ("\nPlease enter your address (example : 123 Barber Street) : ")
@@ -27,62 +27,62 @@ def get_user_information():
     phonenumber = input ("\nPlease enter your phone number (example : 555-555-5555) : ")
 
     personalinformation = [name, address, city, state, zipcode, phonenumber]
-    return personalinformation
+    return personalinformation #Return personalinformation csl
 
-def get_directory_name():
+def get_directory_name(): #Function to take user input of directory and create it or check if it exists
 
     directoryinput = input ("\nPlease enter the directory that you would like to save your file to (example : C:\MyInformation) : ")
-    try :
+    try : #Try makes new folder if the folder does not exist
         os.mkdir(directoryinput)
         print (f"\nDirectory : {directoryinput} created...")
-    except : 
+    except : #Except checks to see if folder exists
         print (f"\nOk, that directory already exists, let us continue...")
-    return directoryinput
+    return directoryinput #Return file directory
 
-def get_file_name(directoryinput):
+def get_file_name(directoryinput): #Function to take user input of file name and complete file path
 
     filename = input ("\nPlease enter a file name to save your information to (example : filename) : ")
-    path_file = directoryinput + '/' + filename + ".json"
+    path_file = directoryinput + '/' + filename + ".json" #Create a full folder and file path to file
 
-    try : 
+    try : #Try checks to see if file already exists
         with open (path_file) as f:
             filename = input ("\nThat file already exists, please enter a new file name to save your information to (example : filename) : ")
             path_file = directoryinput + '/' + filename + ".json"
-    except FileNotFoundError:
+    except FileNotFoundError: #Except continues if file does not already exist
         print (f"\nOk, your information will be saved as : {path_file}, let us continue...")
     
-    return path_file
+    return path_file #Returns full folder and file name
 
-def write_file(personalinfo,path_file):
+def write_file(personalinfo,path_file): #Function writes information to file
 
     with open (path_file, 'w') as f :
         json.dump(personalinfo, f)
 
-def open_file(path_file):
+def open_file(path_file): #Function opens file and returns information from file
 
     with open (path_file) as f :
         information = json.load(f)
         return information
 
-def print_information_from_file (information,path_file):
+def print_information_from_file (information,path_file): #Function prints output
 
     print ("\nThe information you provided to be saved is as follows : ")
 
-    for info in information :
+    for info in information : #Print each item in list from file for user to see
         print (info.title())
 
-    print (f"\nYour information has been stored in : {path_file}\n")
+    print (f"\nYour information has been stored in : {path_file}\n") #Lets the user know where it was stored
 
-def main ():
+def main (): #Main function does all the calling and work
 
-    directoryinput = get_directory_name()
-    path_file = get_file_name(directoryinput)
-    personalinfo = get_user_information()
-    write_file(personalinfo,path_file)
-    information = open_file(path_file)
-    print_information_from_file(information,path_file)
+    directoryinput = get_directory_name() #Call get_directory_name and return directoryinput string
+    path_file = get_file_name(directoryinput) #Call get_file_name, passing directoryinput and return path_file string
+    personalinfo = get_user_information() #Call get_user_information and return personalinfo list
+    write_file(personalinfo,path_file) #Call write_file, passing personalinfo and path_file, writes file
+    information = open_file(path_file) #Call open_file, passing path_file and return information, opens file
+    print_information_from_file(information,path_file) #Call print_information_from_file, passing information & path_file, prints output
 
-main()
+main() #Call main for program to function as it was meant to
 
 
 
